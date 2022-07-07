be3d647055f53ab194ea1534f76251b19fadb4dd
Aakash Ramani <98678395+AksRamani@users.noreply.github.com>
2022-07-07 16:39:08 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import { FaInstagram } from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://www.linkedin.com/in/aᴀᴋᴀsʜ-rᴀᴍᴀɴɪ-541321235/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/AksRamani" target="_blank" rel="noreferrer" ><FaGithub /></a>
       <a href="https://www.instagram.com/aks_ramani" target="_blank" rel="noreferrer" ><FaInstagram /></a>
     </div>
   )
